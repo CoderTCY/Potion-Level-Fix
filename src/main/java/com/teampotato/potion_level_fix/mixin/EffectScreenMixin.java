39dd67077c6654fb64987fa8f17c5a2f1344e0f2
KalebLangley <2436262516@qq.com>
2025-08-31 01:22:56 +0800

    使用最新的方式捕获表达式更快的办法

--- src/main/java/com/teampotato/potion_level_fix/mixin/EffectScreenMixin.java
+++ src/main/java/com/teampotato/potion_level_fix/mixin/EffectScreenMixin.java
@@ -1,35 +1,28 @@
 package com.teampotato.potion_level_fix.mixin;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.teampotato.potion_level_fix.PotionLevelFix;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.Tag;
-import net.minecraft.network.chat.CommonComponents;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.effect.MobEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 @Mixin(EffectRenderingInventoryScreen.class)
 public abstract class EffectScreenMixin {
-    @Inject(method = "getEffectName", at = @At(value = "RETURN"), cancellable = true)
-    private void modifyEffectName(MobEffectInstance pEffect, CallbackInfoReturnable<Component> cir) {
-        MutableComponent mutablecomponent = pEffect.getEffect().getDisplayName().copy();
-        int amplifier = pEffect.getAmplifier() + 1;
+    @Definition(id = "effect", local = @Local(argsOnly = true, type = MobEffectInstance.class))
+    @Definition(id = "getAmplifier", method = "Lnet/minecraft/world/effect/MobEffectInstance;getAmplifier()I")
+    @Expression("effect.getAmplifier() <= 9")
+    @ModifyExpressionValue(method = "getEffectName", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean greaterTrue(boolean original) {
+        return true;
+    }
 
-        if (amplifier > 1){
-            Component amplifierText = Component.literal(String.valueOf(amplifier));
-            if (PotionLevelFix.EFFECT_NUMBER.get()) {
-                amplifierText = Component.translatable("enchantment.level." + amplifier);
-            }
-            mutablecomponent.append(CommonComponents.SPACE).append(amplifierText);
-        }
-        cir.setReturnValue(mutablecomponent);
+    @Expression("'enchantment.level.'")
+    @ModifyExpressionValue(method = "getEffectName", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private String hackI18Key(String original) {
+        return PotionLevelFix.EFFECT_NUMBER.get() ? original : "";
     }
 }
